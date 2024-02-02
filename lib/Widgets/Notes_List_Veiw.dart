bebe20c911e515046d2b831d82b0f4a1bdeb0b4f
olov7a <zeadolov@gmail.com>
2024-02-03 01:34:09 +0200

    creat Notes lest view

---
+++ lib/Widgets/Notes_List_Veiw.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:notes/Widgets/Noteitem.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context,index){
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: const Noteitem(),
+      );
+    });
+  }
+}
